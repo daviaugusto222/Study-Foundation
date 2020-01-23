f4e7850a94b6a904fe0c49cb2daeab66d801db4b
Arnaldo Aguiar <arnaldoaguiarp@gmail.com>
2020-01-23 08:30:55 -0300

    Perfil Update

---
+++ Study/EditarPerfilViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditarPerfilViewController.swift
+//  Study
+//
+//  Created by Arnaldo Aguiar on 23/01/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import UIKit
+
+class EditarPerfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Study/PerfilViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PerfilViewController.swift
+//  Study
+//
+//  Created by Arnaldo Aguiar on 23/01/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import UIKit
+
+class PerfilViewController: UIViewController {
+
+    @IBOutlet weak var perfilTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
