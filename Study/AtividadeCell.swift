035824b0f7b70e3f5a6eed85e1b8e1e71e1964b8
David Augusto <daviaugusto222@gmail.com>
2020-01-23 14:01:16 -0300

    Home

---
+++ Study/AtividadeCell.swift
@@ -0,0 +1,27 @@
+//
+//  AtividadeCell.swift
+//  Study
+//
+//  Created by David Augusto on 23/01/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import UIKit
+
+class AtividadeCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
