a69a9a6cb8b43ddf4f01bc55e8ced14ac39a2f09
Jardel Alves <jardelfilho2001@gmail.com>
2020-01-23 16:16:04 -0300

    Finalizada tela de Jornadas

--- Study/JornadaTableViewController.swift
+++ Study/JornadaTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 let jornadasArray = ["Beber água", "Alongar", "Fazer exercícios", "Aprender pomodoro", "Aplicar em alguma cadeira", "Tirar 10 na prova", "Zerar a vida"]
 
+let jornadasDescricaoArray = ["Manter-se hidratado é sempre essencial para a sua saúde.", "Antes de qualquer exercícios, sempre bom alongar-se para evitar dores nos músculos.", "Cuide bem do seu corpo com exercícios físicos. Comece pegando leve, aumentando a carga cada vez que se acostumar com o ritmo.", "Técnica.", ]
+
 var position = 0
 
 class JornadaTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -31,7 +33,6 @@ class JornadaTableViewController: UIViewController, UITableViewDelegate, UITable
         return jornadasArray.count
     }
 
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Jornadas", for: indexPath) as! JornadaCell
         
