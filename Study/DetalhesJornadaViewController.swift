dd3fc79182af6c92a75b6de49780151024b51adc
Jardel Alves <jardelfilho2001@gmail.com>
2020-01-24 10:42:38 -0300

    Finalizada tela de Jornadas

--- Study/DetalhesJornadaViewController.swift
+++ Study/DetalhesJornadaViewController.swift
@@ -11,12 +11,24 @@ import UIKit
 class DetalhesJornadaViewController: UIViewController {
     @IBOutlet weak var nomeJornada: UILabel!
     @IBOutlet weak var descricaoJornada: UILabel!
+    @IBOutlet weak var concluirJornadaButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nomeJornada.text = jornadasArray[position]
-        
+        nomeJornada.text = jornadasArray[position].nome
         descricaoJornada.text = jornadasDescricaoArray[position]
     }
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        for (index, jornada) in jornadasArray.enumerated() {
+            if jornada.nome == nomeJornada.text {
+                jornadasArray[index].concluida = true
+            }
+        }
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
